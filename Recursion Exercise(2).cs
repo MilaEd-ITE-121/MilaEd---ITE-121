6f0c1249e8f7b9b45db02c164c3ee7c92fbf1377
MilaEd-ITE-121 <100063102+MilaEd-ITE-121@users.noreply.github.com>
2022-03-21 09:34:29 +0800

    Add files via upload

---
+++ Recursion Exercise(2).cs
@@ -0,0 +1,40 @@
+﻿
+using System;
+
+class EdzelMilaVowels
+{
+    public static void Main(String[] args)
+    {
+        String str = "abc doefgaa";
+        Console.WriteLine($"string: {str}");
+        Console.WriteLine($"Total number of vowels: {TotalVowels(str, str.Length)}");
+    }
+    static Boolean IsVowel(char ch)
+    {
+        ch = char.ToUpper(ch);
+
+        return (ch == 'A' || ch == 'E' ||
+                ch == 'I' || ch == 'O' ||
+                ch == 'U') == true &&
+                ch >= 65 && ch <= 90;
+    }
+
+    static int TotalVowels(String str, int n)
+    {
+        if (n == 1)
+        {
+            if (IsVowel(str[0]))
+                return 1;
+            else
+                return 0;
+        }
+
+        if (IsVowel(str[n - 1]))
+            return TotalVowels(str, n - 1) + 1;
+        else
+            return TotalVowels(str, n - 1);
+    }
+}
+// code for counting vowels
+
+//code by Edzel Mila BSIT-1
